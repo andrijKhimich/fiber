f269abe527abe49c2f084c9bb0e57725fa2a7622
Andrew Khimich <andrijkhimich@gmail.com>
2023-11-16 18:46:28 +0200

    add new pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,40 @@
+const toggleMenu = () => {
+	const burger = document.querySelector('.js-burger');
+	const menu = document.querySelector('.js-header-nav');
+	const body = document.querySelector('body');
+	burger.addEventListener('click', () => {
+		if (!menu.classList.contains('active')) {
+			menu.classList.add('active');
+			burger.classList.add('active');
+			body.classList.add('locked');
+		} else {
+			menu.classList.remove('active');
+			burger.classList.remove('active');
+			body.classList.remove('locked');
+		}
+	});
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > 992) {
+			menu.classList.remove('active');
+			burger.classList.remove('active');
+			body.classList.remove('locked');
+		}
+	});
+};
+toggleMenu();
+
+function fixedHeader() {
+	const header = document.querySelector('.js-header');
+	let sticky = 100;
+	if (window.scrollY > sticky) {
+		header.classList.add('fixed');
+	} else {
+		header.classList.remove('fixed');
+	}
+}
+
+window.addEventListener('scroll', fixedHeader);
+
 !(function () {
 	setTimeout(() => {
 		const blocks = document.querySelectorAll('.in-view-detect');
